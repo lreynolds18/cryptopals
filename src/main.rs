a0e42275325a26ea4a4b4a2dc01567299d57774a
Lucas Reynolds <lreynolds18@gmail.com>
2018-06-05 20:24:13 -0400

    got single byte xor cipher working :D

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ pub mod set1;
 fn main() {
   // TODO: change this to go through every set and challenge
 
-  // Test 6
-  println!("Test 6 - Test hex to base64");
+  // Set 1, Challenge 1
+  println!("Set 1, Challenge 1 - hex to base64");
   println!("Input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
   println!("Ans: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
   println!("Res: {}", set1::hex_to_base64(&"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string())); 
   println!();
 
-  // Test 7
-  println!("Test 7 - Test fixed XOR");
+  // Set 1, Challenge 2
+  println!("Set 1, Challenge 2 - fixed XOR");
   println!("Input: 1c0111001f010100061a024b53535009181c");
   println!("Input: 686974207468652062756c6c277320657965");
   println!("Ans: 746865206b696420646f6e277420706c6179");
@@ -19,25 +19,25 @@ fn main() {
                                       &"686974207468652062756c6c277320657965".to_string(), &"hex".to_string()));
   println!();
 
-  // Test 8
-  println!("Test 8 - Test Single-byte XOR cipher");
+  // Set 1, Challenge 3
+  println!("Set 1, Challenge 3 - Single-byte XOR cipher");
   println!("Input: 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
   let (ans_message, ans_key) = set1::single_byte_xor_cipher(&"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".to_string(), &"hex".to_string());
-  println!("Ans message: ?");
+  println!("Ans message: Cooking MC's like a pound of bacon");
   println!("Res message: {}", ans_message);
-  println!("Ans key: ?");
+  println!("Ans key: X");
   println!("Res key: {}", ans_key); 
   println!();
 
-  // Test 9
-  println!("Test 9 - Test Detect single-character XOR");
+  // Set 1, Challenge 4 
+  println!("Set 1, Challenge 4 - Detect single-character XOR");
   println!("Input File: set1challenge4.txt");
   println!("Ans: ?");
   println!("Res: {}", set1::detect_single_character_xor("./set1challenge4.txt"));
   println!();
 
-  // Test 10
-  println!("Test 10 - Test repeating-key XOR");
+  // Set 1, Challenge 5
+  println!("Set 1, Challenge 5 - repeating-key XOR");
   println!("Input String: Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal");
   println!("Input key: ICE");
   println!("Ans: 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
--- src/set1/mod.rs
+++ src/set1/mod.rs
@@ -15,6 +15,7 @@ pub fn hex_to_base64(hex_str: &str) -> String {
   s.to_string()
 }
 
+
 /* fixed_xor -- Set 1, Challenge 2
  * http://cryptopals.com/sets/1/challenges/2
  * xor on two hex strings 
@@ -33,6 +34,22 @@ pub fn fixed_xor(lhs_str: &str, lhs_type: &str, rhs_str: &str, rhs_type: &str) -
   ans.to_string()
 }
 
+
+/* char_freq -- helper function that returns the character frequency
+ * Parameters: str_inp (&str) - input string (ascii)
+ * Return: f64 - character frequency score
+ */
+pub fn char_freq(str_inp: &str) -> i32 {
+  let mut count: i32 = 0;
+  for c in str_inp.chars() {
+    if c.is_alphanumeric() || c == ' ' || c == '\'' {
+      count += 1;
+    }
+  }
+  count
+}
+
+
 /* single_byte_xor_cipher -- Set 1, Challenge 3
  * http://cryptopals.com/sets/1/challenges/3
  * The hex string has been XOR'd against a single character. Find the key, decrypt the message.  
@@ -40,9 +57,30 @@ pub fn fixed_xor(lhs_str: &str, lhs_type: &str, rhs_str: &str, rhs_type: &str) -
  * Return: (String, Char) - (hidden message, key that was used)
  */
 pub fn single_byte_xor_cipher(str_inp: &str, str_type: &str) -> (String, char) {
+  let s = storage::Storage::new(&str_inp, &str_type);
 
-  ("".to_string(), 'A')
+  let mut result_string: String = s.to_string();
+  let mut result_char: char = '0';
+  let mut max_freq: i32 = 0;
+  let mut tmp_freq: i32;
+
+  for i in "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".chars() {
+    let mut char_obj = storage::Storage::new(&i.to_string(), &"ascii");  
+    char_obj.change_base(&"hex".to_string());
+    let mut ans = &s ^ &char_obj;
+    ans.change_base(&"ascii".to_string());
+    
+    tmp_freq = char_freq(&ans.to_string().as_str());
+    if tmp_freq > max_freq {
+      result_string = ans.to_string();
+      result_char = i;
+      max_freq = tmp_freq;
+    }
+  }
+
+  (result_string, result_char) 
 }
+
 
 /* detect_single_character_xor -- Set 1, Challenge 4
  * http://cryptopals.com/sets/1/challenges/4
@@ -55,6 +93,7 @@ pub fn detect_single_character_xor(filename: &str) -> (char) {
   // ("".to_string(), 'A')
   'A'
 }
+
 
 /* repeating_key_xor_encrypt -- Set 1, Challenge 5
  * http://cryptopals.com/sets/1/challenges/5
@@ -75,5 +114,4 @@ pub fn repeating_key_xor_encrypt(lhs_str: &str, lhs_type: &str, rhs_str: &str, r
 
   ans.to_string()
 }
-
 
